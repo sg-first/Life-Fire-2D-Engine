32f78769d70caa651080f3a63c7c4a192b1e2d62
3A <524019351@qq.com>
2015-08-01 12:47:23 +0800

    Beta v2.56

--- maincall.cpp
+++ maincall.cpp
@@ -3,14 +3,5 @@
 
 void maincall::StartGame()//入口函数
 {
-    /*QVector<QString>addr;
-    addr<<"D:/梦影测试/陈的混混/2.jpg";
-    addr<<"D:/梦影测试/陈的混混/1.jpg";
-    addr<<"D:/梦影测试/陈的混混/3.jpg";
-    //addr<<"D:/梦影测试/陈的混混/2.jpg";
-    AddPicAnimation(addr,0,0,700);
-    AddTextItem("C++是垃圾语言","宋体",20,0,0,230,0,0);*/
-    QPixmap *pixmap=NewQPixmap("D:/梦影测试/陈的混混/2.jpg");
-    AddPixmapItem(pixmap,0,0);
-    AddPixmapItem(pixmap,200,300);
+
 }
